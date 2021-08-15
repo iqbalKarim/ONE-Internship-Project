32b4e85712a281b3d51690732c162f7fc92b75ec
Iqbal Karim <iqbalkarimsiddiqui2015@gmail.com>
2021-08-15 06:00:53 +0500

    AppuserController works

--- Controllers/AppUserController.cs
+++ Controllers/AppUserController.cs
@@ -1,24 +1,34 @@
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Linq;
 using System.Threading.Tasks;
-using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
+using Microsoft.Data.SqlClient;
 
 namespace API.Controllers
 {
     public class AppUserController : BaseApiController
     {
-        private readonly DataContext _context;
-        public AppUserController(DataContext context)
+        public AppUserController()
         {
-            _context = context;
         }
 
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        [HttpGet("GetUsers")]
+        public List<AppUser> GetUsers()
         {
-            return await _context.Users.ToListAsync();
+            SqlConnection connection = new SqlConnection(@"Data Source=LAPTOP-GMOA06LL\SQLEXPRESS;Initial Catalog=ONE_ProjectDatabase;Trusted_Connection=True");
+            SqlDataAdapter sqlAda = new SqlDataAdapter("Select * from AppUsers",connection);
+            DataTable data = new DataTable();
+            sqlAda.Fill(data);
+
+            List<AppUser> list = new List<AppUser>();
+            list = (from DataRow dr in data.Rows 
+                    select new AppUser(Convert.ToInt32(dr["Id"]), dr["UserName"].ToString())
+                    ).ToList();
+            
+            return list;
         }
     }
 }
--- Entities/AppUser.cs
+++ Entities/AppUser.cs
@@ -2,8 +2,27 @@ namespace API.Entities
 {
     public class AppUser
     {
-        public int Id { get; set; }
-        public string UserName { get; set; }
+        private int id;
+        private string username;
+        public AppUser(int Id, string UserName){
+            this.Id = Id;
+            this.UserName = UserName;
+        }
+        public int Id { 
+            get{
+                return id;
+            } 
+            set{
+                id = value;
+            } 
+        }
+        public string UserName { 
+            get{
+                return username;
+            } set{
+                username = value;
+            } 
+        }
         // public string PasswordHash { get; set; }
         // public string PasswordSalt { get; set; }
     }
